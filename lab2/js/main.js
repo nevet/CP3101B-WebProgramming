7963b08c2ca5edeca992d94d3a4359fbec525973
Huang Da <a0091847@nus.edu.sg>
2015-02-01 20:34:14 +0800

    revamped, mss done

--- lab2/js/main.js
+++ lab2/js/main.js
@@ -22,6 +22,7 @@ var lightBrown = "rgb(86, 51, 36)";
 var darkBrown = "rgb(52, 34, 34)";
 var red = "rgb(255, 0, 0)";
 var table = $('table')[0];
+var styledName = ['<span id="indicator">H</span>ome', '<span id="indicator">R</span>iver!', '<span id="indicator">G</span>arden', '<span id="indicator">L</span>ibrary'];
 
 function rgbSum(c1, c2) {
   var output = "rgb(" + Math.floor((c1.r + c2.r) / 2) + ", " + Math.floor((c1.g + c2.g) / 2) + ", " + Math.floor((c1.b + c2.b) / 2) + ")";
@@ -42,116 +43,141 @@ function clickOnAdjcent(cell) {
 }
 
 function clickOnVisited(cell) {
-  var visited = cell.text().match('/^(?:[HRGL]\/)?(\d+)$/');
+  var cellText = cell.text();
+  var visited = cellText.match(/^(?:[HRGL]\/)?(\d+)$/);
 
   return visited != null ? parseInt(visited[1], 10) : -1;
 }
 
-function recoverCell(cell) {
+function setGameState(state) {
+  gameState = state;
+
+  switch (state) {
+  case 0:
+    return 'Start from <span id="indicator">H</span>ome :)';
+  case 1:
+    return 'Good start! We are now heading to <span id="indicator">R</span>iver!';
+  case 2:
+    return 'Good job! We are now heading to <span id="indicator">G</span>arden!';
+  case 3:
+    return 'Well done! We are now heading to <span id="indicator">L</span>ibrary!';
+  case 4:
+    return 'Almost there! We are now heading <span id="indicator">H</span>ome!';
+  }
+}
+
+function setInstruction(cell) {
+  var instruction = $('#instruction');
+
+  if (cell.text() == '.') {
+    instruction.html('Moving towards ' + styledName[gameState]);
+  } else {
+    instruction.html(setGameState(gameState));
+  }
+}
+
+function setStepCount(count) {
+  stepCount = count;
+  $('#step-count').text(stepCount);
+}
+
+function switchCellStatus(cell, highlight) {
   var r = parseInt(cell.attr('r'), 10) - 1;
   var c = parseInt(cell.attr('c'), 10) - 1;
+  beforeHoverBg = highlight ? red : ((r + c) % 2 == 0 ? lightBrown : darkBrown);
 
-  cell.text(map[origraph[r][c]]);
   cell.css({
-    "background": (r + c) % 2 == 0 ? lightBrown : darkBrown
+    'background': beforeHoverBg
   });
 
-  stepCount --;
+  if (highlight) {
+    if (origraph[r][c] < 4) {
+      cell.text(cell.text() + '/' + stepCount);
+    } else {
+      cell.text(stepCount);
+    }
+  } else {
+    cell.text(map[origraph[r][c]]);
+  }
+
+  lastClick = {'r': r + 1, 'c': c + 1};
 }
 
 function verify(cell) {
-  if (cell.text() == '#') return 'Error: On obstacle!';
+  if (cell.text() == '#') return {'status': 'err', 'msg': 'On obstacle!'};
 
-  // if user clicks on some non-adjacent cell, invalid 
-  if (!clickOnAdjcent(cell)) return 'Error: Cats cannot fly!';
-
-  // else, we have 3 cases:
-  // 1. he clicks on some visited cell
-  //  1.1 that cell is where he comes
-  //  1.2 that cell is a cell on the path
-  // 2. a new adjacent cell
+  if (!clickOnAdjcent(cell)) return {'status': 'err', 'msg': 'Cats cannot fly!'};
 
   var cellStepCount = clickOnVisited(cell);
 
   if (cellStepCount == stepCount) {
-    // case 1.1
-    recoverCell(cell);
-    return '';
+    return {'status': 'undo', 'msg': ''};
   } else
   if (cellStepCount != -1) {
-    // case 1.2
-    return 'Error: Cats cannot visit any non-home cell twice!';
+    return {'status': 'err', 'msg': 'Cats cannot walk across their path!'};
   }
 
-  // case 2
   switch (gameState) {
     case 0:
       // we just started the game, we can only click on 'H'
       if (cell.text() != 'H') {
-        return 'Error: We should start from <span id="indicator">H</span>ome';
+        return {'status': 'err', 'msg': 'We should start from <span id="indicator">H</span>ome'};
       } else {
-        gameState = 1;
-        return 'Good start! We are now heading to <span id="indicator">R</span>iver!';
+        return {'status': 'ok', 'msg': setGameState(1)};
       }
       break;
     case 1:
       // we are heading to 'R'
       if (cell.text() == 'R') {
-        gameState = 2;
-
-        return 'Good job! We are now heading to <span id="indicator">G</span>arden!';
+        return {'status': 'ok', 'msg': setGameState(2)};
       } else {
         if (cell.text() == '.') {
-          return 'Moving towards <span id="indicator">R</span>iver!';
+          return {'status': 'ok', 'msg': 'Moving towards <span id="indicator">R</span>iver!'};
         }
 
-        return 'Error: We should visit <span id="indicator">R</span>iver first!';
+        return {'status': 'err', 'msg': 'We should visit <span id="indicator">R</span>iver first!'};
       }
       break;
     case 2:
       // we are heading to 'G'
       if (cell.text() == 'G') {
-        gameState = 3;
-
-        return 'Well done! We are now heading to <span id="indicator">L</span>ibrary!';
+        return {'status': 'ok', 'msg': setGameState(3)};
       } else {
         if (cell.text() == '.') {
-          return 'Moving towards <span id="indicator">G</span>arden!';
+          return {'status': 'ok', 'msg': 'Moving towards <span id="indicator">G</span>arden!'};
         }
 
-        return 'Error: We should visit <span id="indicator">G</span>arden first!';
+        return {'status': 'err', 'msg': 'We should visit <span id="indicator">G</span>arden first!'};
       }
       break;
     case 3:
       // we are heading to 'L'
       if (cell.text() == 'L') {
-        gameState = 4;
-
-        return 'Almost there! We are now heading <span id="indicator">H</span>ome!';
+        return {'status': 'ok', 'msg': setGameState(4)};
       } else {
         if (cell.text() == '.') {
-          return 'Moving towards <span id="indicator">L</span>ibrary!';
+          return {'status': 'ok', 'msg': 'Moving towards <span id="indicator">L</span>ibrary!'};
         }
 
-        return 'Error: We should visit <span id="indicator">L</span>ibrary first!';
+        return {'status': 'err', 'msg': 'We should visit <span id="indicator">L</span>ibrary first!'};
       }
       break;
     case 4:
-      // we are heading to 'H' {
+      // we are heading to 'H'
       if (cell.text() == 'H/1') {
         gameState = 0;
 
         alert('You have completed in ' + stepCount + ' moves!' + getScore());
       } else {
         if (cell.text()) {
-          return 'Moving towards <span id="indicator">H</span>ome!';
+          return {'status': 'ok', 'msg': 'Moving towards <span id="indicator">H</span>ome!'};
         }
 
-        return 'Error: We should visit <span id="indicator">H</span>ome!';
+        return {'status': 'err', 'msg': 'We should visit <span id="indicator">H</span>ome'};
       }
   }
-  
-  return "Success";
+
+  return {'status': 'idle', 'msg': ''};
 }
 
 function init() {
@@ -281,34 +307,26 @@ $(function () {
 
     var msg = verify(cell);
 
-    if (msg.length) {
-      instruction.html(msg);
-    } else {
-      $('#step-count').text(stepCount);
-      return;
-    }
-    
-    if (msg.indexOf('Error') == -1) {
-      stepCount ++;
-      lastClick = {"r": parseInt(cell.attr('r')), "c": parseInt(cell.attr('c'))};
+    if (msg.status == 'err') {
+      instruction.addClass('error');
+      instruction.html(msg.msg);
+    } else
+    if (msg.status == 'ok') {
+      instruction.addClass('success');
+      instruction.html(msg.msg);
 
-      $('#instruction').addClass('success');
-      if (cell.text() != '.') {
-        cell.text(cell.text() + '/' + stepCount);
-      } else {
-        cell.text(stepCount);
+      setStepCount(stepCount + 1);
+      switchCellStatus(cell, true);
+    } else
+    if (msg.status == 'undo') {
+      switchCellStatus(cell, false);
+      setStepCount(stepCount - 1);
+      
+      if (origraph[cell.attr('r') - 1][cell.attr('c') - 1] < 4) {
+        gameState --;
       }
-    } else {
-      $('#instruction').addClass('error');
-      return;
+
+      setInstruction(cell);
     }
-
-    beforeHoverBg = red;
-    
-    cell.css({
-      "background": beforeHoverBg
-    });
-
-    $('#step-count').text(stepCount);
   });
 });
