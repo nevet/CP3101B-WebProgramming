52b3cf2784b0b642f80f9d187d9a6fb39adf5364
Huang Da <a0091847@nus.edu.sg>
2015-03-03 04:16:52 +0800

    update .gitignore to ignore config files, also remove config.php from repo

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php // config.php basically contains these 4 constants
-  define("db_host", "localhost");
-  define("db_uid", "root");
-  define("db_pwd", "");
-  define("db_name", "wanderingCat");
-?>
